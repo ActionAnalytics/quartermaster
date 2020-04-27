d2f397b0b13ef760e79486407935b2d32a16c41d
Shea Phillips <shea.phillips@gmail.com>
2020-04-27 14:40:41 -0700

    CI/CD and build tweaks

--- app/.pipeline/lib/config.js
+++ app/.pipeline/lib/config.js
@@ -1,14 +1,50 @@
 'use strict';
-const options= require('pipeline-cli').Util.parseArguments()
+const options = require('pipeline-cli').Util.parseArguments()
 const changeId = options.pr //aka pull-request
 const version = '1.0.0'
 const name = 'cloud-navigator'
 
 const phases = {
-  build: {namespace:'tnfhhm-tools'    , name: `${name}`, phase: 'build'  , changeId:changeId, suffix: `-build-${changeId}`  , instance: `${name}-build-${changeId}`  , version:`${version}-${changeId}`, tag:`build-${version}-${changeId}`},
-  dev: {namespace:'tnfhhm-dev'    , name: `${name}`, phase: 'dev'  , changeId:changeId, suffix: `-dev-${changeId}`  , instance: `${name}-dev-${changeId}`  , version:`${version}-${changeId}`, tag:`dev-${version}-${changeId}`},
-  test: {namespace:'tnfhhm-test'    , name: `${name}`, phase: 'test'  , changeId:changeId, suffix: `-test`  , instance: `${name}-test`  , version:`${version}`, tag:`test-${version}`},
-  prod: {namespace:'tnfhhm-prod'    , name: `${name}`, phase: 'prod'  , changeId:changeId, suffix: `-prod`  , instance: `${name}-prod`  , version:`${version}`, tag:`prod-${version}`},
+  build: {
+    namespace: 'tnfhhm-tools',
+    name: `${name}`,
+    phase: 'build',
+    changeId: changeId,
+    suffix: `-build-${changeId}`,
+    instance: `${name}-build-${changeId}`,
+    version: `${version}-${changeId}`,
+    tag: `build-${version}-${changeId}`
+  },
+  dev: {
+    namespace: 'tnfhhm-dev',
+    name: `${name}`,
+    phase: 'dev',
+    changeId: changeId,
+    suffix: `-dev-${changeId}`,
+    instance: `${name}-dev-${changeId}`,
+    version: `${version}-${changeId}`,
+    tag: `dev-${version}-${changeId}`
+  },
+  test: {
+    namespace: 'tnfhhm-test',
+    name: `${name}`,
+    phase: 'test',
+    changeId: changeId,
+    suffix: `-test`,
+    instance: `${name}-test`,
+    version: `${version}`,
+    tag: `test-${version}`
+  },
+  prod: {
+    namespace: 'tnfhhm-prod',
+    name: `${name}`,
+    phase: 'prod',
+    changeId: changeId,
+    suffix: `-prod`,
+    instance: `${name}-prod`,
+    version: `${version}`,
+    tag: `prod-${version}`
+  },
 };
 
 // This callback forces the node process to exit as failure.
